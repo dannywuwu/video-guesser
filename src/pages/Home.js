debbfae92bb10e0c6c522c93c13ab9ea97801f93
weelam <tao.lin6852@gmail.com>
2021-09-05 21:43:06 -0400

    prettier reformatted everything

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,108 +12,108 @@ import { formatCountdown } from 'antd/lib/statistic/utils';
 const { Title } = Typography;
 
 function Home() {
-    const history = useHistory()
-    const { room, setRoom } = useRoom().room
-    const { name, setName } = useRoom().name
+	const history = useHistory()
+	const { room, setRoom } = useRoom().room
+	const { name, setName } = useRoom().name
 
-    const [isModalVisible, setIsModalVisible] = useState(false)
-    const [isJoin, setIsJoin] = useState(false) // joining or creating?
+	const [isModalVisible, setIsModalVisible] = useState(false)
+	const [isJoin, setIsJoin] = useState(false) // joining or creating?
 
-    const showCreate = () => {
-        setIsModalVisible(true);
-        setIsJoin(false)
-    };
+	const showCreate = () => {
+		setIsModalVisible(true);
+		setIsJoin(false)
+	};
 
-    const showJoin = () => {
-        setIsModalVisible(true);
-        setIsJoin(true)
-    };
+	const showJoin = () => {
+		setIsModalVisible(true);
+		setIsJoin(true)
+	};
 
-    const onFinish = (value) => {
-        setIsModalVisible(false);
-        // redirect to Game page, and then connect them to the socket
-        if (isJoin) console.log('these nuts')
-        else {
-            setRoom(value.user.room)
-            setName(value.user.name)
-            history.push('/game')
-        }
-    };
-    const handleCancel = () => {
-        setIsModalVisible(false);
-    };
-    return (
-        <Space direction="vertical" align="center" style={{ height: "100vh" }}>
-            <Title level={2}>Game Title</Title>
-            <Button type="primary" shape="round" size="large" onClick={showJoin}> Join Game </Button>
-            <Button type="primary" shape="round" size="large" onClick={showCreate}> Create Game </Button>
+	const onFinish = (value) => {
+		setIsModalVisible(false);
+		// redirect to Game page, and then connect them to the socket
+		if (isJoin) console.log('these nuts')
+		else {
+			setRoom(value.user.room)
+			setName(value.user.name)
+			history.push('/game')
+		}
+	};
+	const handleCancel = () => {
+		setIsModalVisible(false);
+	};
+	return (
+		<Space direction="vertical" align="center" style={{ height: "100vh" }}>
+			<Title level={2}>Game Title</Title>
+			<Button type="primary" shape="round" size="large" onClick={showJoin}> Join Game </Button>
+			<Button type="primary" shape="round" size="large" onClick={showCreate}> Create Game </Button>
 
-            <Modal
-                visible={isModalVisible}
-                onCancel={handleCancel}
-                footer={[
-                    <Button form="myForm" key="submit" htmlType="submit">
-                        Submit
-                    </Button>
-                ]}
-            >
-                {isJoin
-                    ? (
-                        <Form
-                            id='myForm'
-                            labelCol={{ span: 8 }}
-                            wrapperCol={{ span: 16 }}
-                            initialValues={{ remember: true }}
-                            onFinish={onFinish}
-                            // onFinishFailed={handleFinishFailed}
-                            autoComplete="off"
-                        >
-                            <Form.Item
-                                name={['user', 'name']}
-                                rules={[{ required: true, message: 'Please input your display name!' }]}
-                            >
-                                <Input placeholder="Display name" />
-                            </Form.Item>
-                            <Form.Item
-                                name={['user', 'room']}
-                                rules={[{ required: true, message: 'Please input the room name!' }]}
-                            >
-                                <Input placeholder="Room name" />
-                            </Form.Item>
-                        </Form>
+			<Modal
+				visible={isModalVisible}
+				onCancel={handleCancel}
+				footer={[
+					<Button form="myForm" key="submit" htmlType="submit">
+						Submit
+					</Button>
+				]}
+			>
+				{isJoin
+					? (
+						<Form
+							id='myForm'
+							labelCol={{ span: 8 }}
+							wrapperCol={{ span: 16 }}
+							initialValues={{ remember: true }}
+							onFinish={onFinish}
+							// onFinishFailed={handleFinishFailed}
+							autoComplete="off"
+						>
+							<Form.Item
+								name={['user', 'name']}
+								rules={[{ required: true, message: 'Please input your display name!' }]}
+							>
+								<Input placeholder="Display name" />
+							</Form.Item>
+							<Form.Item
+								name={['user', 'room']}
+								rules={[{ required: true, message: 'Please input the room name!' }]}
+							>
+								<Input placeholder="Room name" />
+							</Form.Item>
+						</Form>
 
-                    )
-                    : (
-                        <Form
-                            id='myForm'
-                            labelCol={{ span: 8 }}
-                            wrapperCol={{ span: 16 }}
-                            initialValues={{ remember: true }}
-                            onFinish={onFinish}
-                            // onFinishFailed={handleFinishFailed}
-                            autoComplete="off"
-                        >
-                            <Form.Item
-                                name={['user', 'name']}
-                                rules={[{ required: true, message: 'Please input your display name!' }]}
-                            >
-                                <Input placeholder="Display name" />
-                            </Form.Item>
-                            <Form.Item
-                                name={['user', 'room']}
-                                rules={[{ required: true, message: 'Please input the room name!' }]}
-                            >
-                                <Input placeholder="Room name" />
-                            </Form.Item>
+					)
+					: (
+						<Form
+							id='myForm'
+							labelCol={{ span: 8 }}
+							wrapperCol={{ span: 16 }}
+							initialValues={{ remember: true }}
+							onFinish={onFinish}
+							// onFinishFailed={handleFinishFailed}
+							autoComplete="off"
+						>
+							<Form.Item
+								name={['user', 'name']}
+								rules={[{ required: true, message: 'Please input your display name!' }]}
+							>
+								<Input placeholder="Display name" />
+							</Form.Item>
+							<Form.Item
+								name={['user', 'room']}
+								rules={[{ required: true, message: 'Please input the room name!' }]}
+							>
+								<Input placeholder="Room name" />
+							</Form.Item>
 
 
-                        </Form>
-                    )
-                }
+						</Form>
+					)
+				}
 
-            </Modal>
-        </Space>
-    );
+			</Modal>
+		</Space>
+	);
 }
 
 export default Home;
