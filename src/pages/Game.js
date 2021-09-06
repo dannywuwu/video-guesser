94a01950ba220bf919689cfd488a3140e4ceb943
dannywuwu <d246wu@gmail.com>
2021-09-05 21:52:57 -0400

    Game.js formatting

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,86 +1,92 @@
-import React, { useState, useEffect } from 'react';
-import { io } from 'socket.io-client'
-import { useRoom } from '../context/RoomProvider.js';
+import React, { useState, useEffect } from "react";
+import { io } from "socket.io-client";
+import { useRoom } from "../context/RoomProvider.js";
 // import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import { Form, Input, Button, Checkbox, Card, Avatar, Row, Col, Box, Space } from 'antd';
-import 'antd/dist/antd.css';
+import {
+  Form,
+  Input,
+  Button,
+  Checkbox,
+  Card,
+  Avatar,
+  Row,
+  Col,
+  Box,
+  Space,
+} from "antd";
+import "antd/dist/antd.css";
 
 const { Meta } = Card;
 
 const classes = {
   gridStyle: {
-    width: '50%',
-    height: 'auto',
-    textAlign: 'center',
+    width: "50%",
+    height: "auto",
+    textAlign: "center",
     marginLeft: "auto",
-    marginRight: "auto"
-  }
-}
+    marginRight: "auto",
+  },
+};
 
-function Game() {
-  const { room } = useRoom().room
-  const { name } = useRoom().name
+const Game = () => {
+  const { room } = useRoom().room;
+  const { name } = useRoom().name;
 
-  const [users, setUsers] = useState([name])
-  const [id, setId] = useState()
-  const [search, setSearch] = useState("")
-  const [submitted, setSubmitted] = useState(false)
-  const [query, setQuery] = useState({})
+  const [users, setUsers] = useState([name]);
+  const [id, setId] = useState();
+  const [search, setSearch] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+  const [query, setQuery] = useState({});
 
-  const [isChooser, setIsChooser] = useState(true)
-  const [isConfigured, setIsConfigured] = useState(false)
-  const [configVisible, setConfigVisible] = useState(true)
+  const [isChooser, setIsChooser] = useState(true);
+  const [isConfigured, setIsConfigured] = useState(false);
+  const [configVisible, setConfigVisible] = useState(true);
 
   const handleFinish = (values) => {
-    console.log(values.search)
-    setSearch(values.search)
-
-  }
+    console.log(values.search);
+    setSearch(values.search);
+  };
   const handleFinishFailed = (e) => {
-    console.log("Finished Failed")
-  }
-  
-  useEffect(() => {
-    const socket = io("https://song-searcher-backend-thing.weelam.repl.co")
+    console.log("Finished Failed");
+  };
 
-    socket.on('connect', () => {
-      console.log(socket.id)
-      setId(socket.id)
-    })
+  useEffect(() => {
+    const socket = io("https://song-searcher-backend-thing.weelam.repl.co");
+
+    socket.on("connect", () => {
+      console.log(socket.id);
+      setId(socket.id);
+    });
 
     socket.emit("join-room", name, room, (user) => {
-      console.log(user)
-    })
+      console.log(user);
+    });
 
     return () => {
-      socket.disconnect()
-      socket.off()
-    }
-  }, [])
+      socket.disconnect();
+      socket.off();
+    };
+  }, []);
 
   // useEffect(() => {
   //   socket.on("display-users", (payload) => {
   //     console.log("Users: " + payload)
   //     // setUsers(prev => [...prev, ...users])
   //   })
-  // }, [users]) 
-
-
+  // }, [users])
 
   useEffect(() => {
     if (search != "") {
       fetch(`https://song-searcher-backend-thing.weelam.repl.co/get/${search}`)
-        .then(res => res.json())
-        .then(data => {
-          console.log(data)
-          setQuery(data)
-          setSubmitted(true)
-
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          setQuery(data);
+          setSubmitted(true);
         })
-        .catch(err => console.log(err))
-
+        .catch((err) => console.log(err));
     }
-  }, [search])
+  }, [search]);
 
   return (
     <div>
@@ -101,7 +107,9 @@ function Game() {
             >
               <Form.Item
                 name="search"
-                rules={[{ required: true, message: 'Please input your username!' }]}
+                rules={[
+                  { required: true, message: "Please input your username!" },
+                ]}
               >
                 <Input placeholder="Search..." />
               </Form.Item>
@@ -117,25 +125,31 @@ function Game() {
                           hover="true"
                           title={item.title}
                           style={classes.gridStyle}
-                          cover={<img alt="example" src={item.url} style={{ height: "100%" }} />}              >
+                          cover={
+                            <img
+                              alt="example"
+                              src={item.url}
+                              style={{ height: "100%" }}
+                            />
+                          }
+                        >
                           <Meta
-                            avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                            avatar={
+                              <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+                            }
                             title={item.channelTitle}
                           />
                         </Card>
                       </div>
                     </Col>
-                  )
-                })
-              }
-
+                  );
+                })}
             </Row>
           </>
-        )}
-
+        )
+      }
     </div>
   );
-
-}
+};
 
 export default Game;
