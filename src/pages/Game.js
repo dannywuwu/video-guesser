6d3f519086682e5593f852a23439b342f0bb8247
dannywuwu <d246wu@gmail.com>
2021-09-09 22:57:58 -0400

    game choose chooser wip

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,14 +1,18 @@
 import React, { useState, useEffect } from "react";
+import { useSocket } from "../context/SocketProvider";
+import VideoPlayer from "../components/VideoPlayer";
 
 const Game = () => {
   const [search, setSearch] = useState("");
   // dictionary playerID -> guess
   const [guess, setGuess] = useState("");
-  const [chooserID, setChooserID] = useState("");
+  const [chooser, setChooser] = useState({});
 
   // phase toggle: 'search', 'guess', 'score'
   // initially search
   const [phase, setPhase] = useState("search");
+
+  const socket = useSocket();
 
   // clear state for next round
   const nextRound = () => {
@@ -19,8 +23,24 @@ const Game = () => {
 
   // mutate users in context
   const chooseChooser = () => {
-    // use modulus %, emit to server
-    // server will broadcast and update the other users' state
+    setChooser(None);
+  };
+
+  // on mount round 0, choose initial chooser
+  useEffect(() => {
+    socket.emit("choose-chooser", roomID);
+  }, []);
+
+  // listen to above emit
+  useEffect(() => {
+    socket.once("chooser-chosen", (newChooser) => {
+      setChooser(newChooser);
+    });
+  }, [chooser]);
+
+  // return true if current user is chooser
+  const isChooser = () => {
+    return socket.id === chooser.id ? true : false;
   };
 
   // give points to selected players
@@ -42,6 +62,19 @@ const Game = () => {
 
   return (
     <div>
+      <h1>chooser is {chooser.id}</h1>
+      <VideoPlayer
+        style={{
+          visibility: isChooser() || phase === "score" ? "visible" : "hidden",
+        }}
+      />
+      <h3
+        style={{
+          visibility: isChooser() ? "hidden" : "visible",
+        }}
+      >
+        if you are not a chooser you can see this
+      </h3>
       <h1>game</h1>
     </div>
   );
