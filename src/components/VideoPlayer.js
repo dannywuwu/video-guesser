699f9088e0fdd21c306b1792663a8715c6194571
Danny Wu <56970917+dannywuwu@users.noreply.github.com>
2021-09-28 22:10:04 -0400

    Update VideoPlayer.js
    reformat later to VideoContainer (since it contains both search + video player)

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactPlayer from "react-player/youtube";
 import SearchContainer from "./SearchContainer";
 
+// TODO: rename to VideoContainer
 const VideoPlayer = (props) => {
   const { url, searchPhase } = props;
   return (
