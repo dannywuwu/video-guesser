44e80e85f7c856e3eac13627ff6f2606157d72af
dannywuwu <d246wu@gmail.com>
2021-10-03 22:50:41 -0400

    UserList/Card selection wip

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -2,13 +2,17 @@ import React from "react";
 import { Card } from "antd";
 import "../styles/ant-components.css";
 
+const toggleSelect = () => {
+  console.log("selected");
+};
+
 const UserCard = (props) => {
   const { name, points, guess } = props;
 
   // Meta component for card
   const { Meta } = Card;
   return (
-    <Card title={`${guess}`} hoverable>
+    <Card title={`${guess}`} hoverable onClick={() => toggleSelect}>
       <Meta title={name} description={`Points: ${points}`} />
     </Card>
   );
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,27 +1,48 @@
 import React from "react";
 import UserCard from "./UserCard";
 
-import { Col, Row } from "antd";
+import { Col, Row, Button } from "antd";
 
 const UserList = (props) => {
-  const { users } = props;
+  const { users, phase, submitSelected } = props;
+
+  // user.id -> selected status (bool)
+  const selectedUsers = {};
+
+  // mutates selectedUsers, !bool
+  const handleClick = (user) => {
+    // negate if exist, else set true
+    if (selectedUsers[user.id]) {
+      selectedUsers[user.id] = !selectedUsers[user.id];
+    } else {
+      selectedUsers[user.id] = true;
+    }
+  };
   return (
-    <div className="site-card-wrapper">
-      <Row gutter={12}>
-        {users.map((user) => {
-          return (
-            <Col span={4}>
-              <UserCard
-                key={user.id}
-                name={user.name}
-                points={user.points}
-                guess={user.guess}
-              />
-            </Col>
-          );
-        })}
-      </Row>
-    </div>
+    <>
+      <div className="site-card-wrapper">
+        <Row gutter={12}>
+          {users.map((user) => {
+            return (
+              <Col span={4} key={user.id}>
+                <UserCard
+                  key={user.id}
+                  name={user.name}
+                  points={user.points}
+                  guess={user.guess}
+                  onClick={() => handleClick(user)}
+                />
+              </Col>
+            );
+          })}
+        </Row>
+      </div>
+      {phase === "score" && (
+        <Button type="primary" onClick={submitSelected}>
+          Submit
+        </Button>
+      )}
+    </>
   );
 };
 
